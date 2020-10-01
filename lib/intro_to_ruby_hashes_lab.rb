be65dcd7a842d98083ec62bb66ac2e4081012df2
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-10-01 16:55:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
-def my_hash_creator(name, "Grace Hopper")
-    #{name: "Grace Hopper"}
-    
+def my_hash_creator(key, value)
+    return {name: "Grace Hopper"}
+  
   # return a hash that includes the key and value parameters passed into this method
 end
 
